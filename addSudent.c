52811a1bc8f6ff89fba863426cd54b69f28fc943
Mohamed Yousry <mmyrali1@gmail.com>
2023-12-14 15:07:12 +0200

    Adding two function for getting the age of the student and the gender ..  gettig the name of the student was there in the last commit

--- addSudent.c
+++ addSudent.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 #include <string.h>
+#include "addSudent.h"
 
 #define MAX_NAME_LENGTH 51 // Maximum length of the name, including the null terminator
 
+
 char studentName[MAX_NAME_LENGTH];
+char studentGender;
+char studentAge;
 
 void addNewStudent()
 {
     system("cls");                          //clear the screen
-
+    ///////////////////////////////////////////////////////////////////////////////
+    // getting student name part
     printf("student's name (maximum 50 characters): ");
     student_name(studentName);
     printf("Student's name: %s\n", studentName);
-
+    ///////////////////////////////////////////////////////////////////////////////
+    // getting student Gender part
     printf("Gender(M/F): ");
-
+    studentGender = getStudentGender();
+    if(studentGender == 'M')
+        printf("Student's gender: Male\n");
+    else
+        printf("Student's gender: Female\n");
+    ///////////////////////////////////////////////////////////////////////////////
+    // getting student Age part
     printf("Age: ");
-
+    studentAge = getStudentAge();
+    printf("Student's age: %d\n", studentAge);
+    ///////////////////////////////////////////////////////////////////////////////
+    // getting student Study year part
     printf("Study year: ");
 }
 
@@ -29,5 +44,61 @@ void student_name(char *name)
     if (length > 0 && name[length - 1] == '\n') {
         name[length - 1] = '\0';
     }
+}
+
+char getStudentGender()
+{
+    char gender;
+    char isValid = 0;
+
+    do {
+        printf("Enter student's gender (M/F): ");
+        gender = getchar();
+
+        // Check if the input is valid
+        if (gender == 'M' || gender == 'm' || gender == 'F' || gender == 'f')
+        {
+            isValid = 1;
+            if (gender == 'm')
+            {
+                gender = 'M';
+            }
+            if (gender == 'f')
+            {
+                gender = 'F';
+            }
+        }
+        else printf("Invalid input! Please enter either 'M' or 'F'.\n");
+
+        // Clear input buffer
+        while (getchar() != '\n'); // Clearing the input buffer
+
+    } while (!isValid);
+
+    return gender;
+}
+
+char getStudentAge()
+{
+    char age;
+    char isValid = 0;
+
+    do {
+        printf("Enter student's age (1-30): ");
+        scanf("%d", &age);
+
+        // Check if the input is within the valid range
+        if (age >= 1 && age <= 30) {
+            isValid = 1;
+        } else {
+            printf("Invalid input! Please enter an age between 1 and 30.\n");
+        }
+
+        // Clear input buffer
+        while (getchar() != '\n'); // Clearing the input buffer
+
+    } while (!isValid);
+
+    return age;
 }
 
--- addSudent.h
+++ addSudent.h
@@ -1,6 +1,11 @@
 #ifndef ADDSUDENT_H_INCLUDED
 #define ADDSUDENT_H_INCLUDED
 
+//Function Decliration
+char getStudentGender();
+void student_name(char *name);
+char getStudentAge();
+
 void addNewStudent();
 
 #endif // ADDSUDENT_H_INCLUDED
